a5eb901fd08080bf428e00a777c090d5f3c53002
Amit Nadir <Amit Nadir>
2017-01-04 18:28:12 +0200

    sample project done

--- examplePrj/ViewController.swift
+++ examplePrj/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import tweetSearchKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    var resultsArr:NSMutableArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+        
+        //Example use of framework
+        let authClass = TwitterOAuthClass(AppConsumerKey: "QiwdLA3iubELTJc70JLjm0USj", AppConsumerSecret: "vQwJSB3p1Grn7X94bETvWp73jA9CSZ4OkZbBXj8gEcjg0zjHhq")
+        
+        authClass.authenticateAndSerach(UserSearchString: "App"){
+            (arr: NSMutableArray) in
+            print("got this array back: \(arr)")
+            self.resultsArr = arr
+            
+            self.tableView.reloadData()
+            
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.isHidden = true
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultsArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = resultsArr[indexPath.row] as? String
+        
+        return cell
+    }
+    
+    
 }
 
---
+++ examplePrj/tweetSearchKit.framework/Headers/tweetSearchKit.h
@@ -0,0 +1,19 @@
+//
+//  tweetSearchKit.h
+//  tweetSearchKit
+//
+//  Created by Amit on 31/12/2016.
+//  Copyright © 2016 mobi-Wiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tweetSearchKit.
+FOUNDATION_EXPORT double tweetSearchKitVersionNumber;
+
+//! Project version string for tweetSearchKit.
+FOUNDATION_EXPORT const unsigned char tweetSearchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tweetSearchKit/PublicHeader.h>
+
+
